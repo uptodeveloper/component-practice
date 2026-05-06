b5a6411d2a9e419cc3e41216c64ed70364feea85
uptodeveloper <144105217+uptodeveloper@users.noreply.github.com>
2026-05-06 22:18:43 +0900

    05 로그인 폼 구현

--- src/exercises/05-LoginFormExercise.jsx
+++ src/exercises/05-LoginFormExercise.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 export default function LoginFormExercise() {
+  //스테이트관리
+
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  //onChange
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  };
+
+  //로그인 버튼 클릭 후 입력값 alert로 보여주면
+  const handleShowFormValueClick = () => {
+    alert(`email: ${form.email}, password: ${form.password}`);
+  };
+  //이메일 , 비밀번호 있을 때만 버튼 활성
+  const isFormFilled = Boolean(form.email && form.password);
+  //네이밍 신경써서
   return (
     <section className="rounded-lg border border-slate-200 bg-white p-5">
       <h2 className="text-xl font-bold">05. 로그인 폼</h2>
       <p className="mt-2 text-sm leading-6 text-slate-600">
-        이메일과 비밀번호를 입력해야 로그인 버튼이 활성화되도록
-        구현해보세요.
+        이메일과 비밀번호를 입력해야 로그인 버튼이 활성화되도록 구현해보세요.
       </p>
 
       <TodoList
@@ -19,15 +37,25 @@ export default function LoginFormExercise() {
 
       <div className="mt-5 grid gap-3">
         <input
+          onChange={handleFormChange}
+          name="email"
+          value={form.email}
           placeholder="이메일"
           className="rounded-md border border-slate-300 px-3 py-2"
         />
         <input
+          onChange={handleFormChange}
+          name="password"
+          value={form.password}
           placeholder="비밀번호"
           type="password"
           className="rounded-md border border-slate-300 px-3 py-2"
         />
-        <button className="rounded-md border border-slate-900 bg-slate-900 px-4 py-2 font-semibold text-white disabled:cursor-not-allowed disabled:border-slate-300 disabled:bg-slate-200 disabled:text-slate-500">
+        <button
+          onClick={handleShowFormValueClick}
+          disabled={!isFormFilled}
+          className="rounded-md border border-slate-900 bg-slate-900 px-4 py-2 font-semibold text-white disabled:cursor-not-allowed disabled:border-slate-300 disabled:bg-slate-200 disabled:text-slate-500"
+        >
           로그인
         </button>
       </div>
